f0a2615034fbc08c0cd4c063b918674b34a6ab99
junjianni <junjian.ni@syngenta.com>
2024-12-31 21:22:48 -0500

    End of Section 3

---
+++ GBSQCMan4-Web/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'GBSQCMan4-Web';
+  users: any;
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
+}
+
